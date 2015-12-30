1d11fa8475ae67d41b4221491a23ede9b15a1f24
Diego Silveira <diego.silveira@ctis.com.br>
2015-12-30 09:06:32 -0200

    Adicionando o resultado do build no repo

---
+++ dist/da-loader.js
@@ -0,0 +1,95 @@
+(function(){
+angular
+    .module('da-loader')
+    .controller('daLoaderController', DaLoaderController);
+
+DaLoaderController.$injector = ['$scope'];
+
+function DaLoaderController(){
+
+}
+
+
+    
+angular
+    .module('da-loader', [ 'ui-router' ] );
+
+
+var version = '0.0.20';
+
+
+LoaderDirective.$inject = ["$rootScope", "LoaderService"];
+angular
+    .module('da-loader')
+    .directive(LoaderDirective);
+
+/* @ngInject */
+function LoaderDirective($rootScope, LoaderService){
+    return {
+        // name: '',
+        // priority: 1,
+        // terminal: true,
+        // scope: {}, // {} = isolate, true = child, false/undefined = no change
+        // controller: function($scope, $element, $attrs, $transclude) {},
+        // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+        restrict: 'AE',
+        // template: '',
+        templateUrl: 'id-loader/loader.html',
+        replace: true,
+        // transclude: true,
+        // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+        link: function($scope, element, attrs, controller) {
+
+            $scope.displayStatus = function(){
+                return LoaderService.isShowing ? 'block' : 'none';
+            }
+
+
+            $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+                LoaderService.show();
+                //event.preventDefault();
+                // transitionTo() promise will be rejected with
+                // a 'transition prevented' error
+                console.log('$stateChangeStart');
+            });
+
+
+            $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error){
+                LoaderService.hide();
+                //event.preventDefault();
+                // transitionTo() promise will be rejected with
+                // a 'transition prevented' error
+                console.log('$stateChangeError');
+            });
+
+
+            $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+                LoaderService.hide();
+                //event.preventDefault();
+                // transitionTo() promise will be rejected with
+                // a 'transition prevented' error
+                console.log('$stateChangeSuccess');
+            });
+
+
+            $rootScope.$on('$stateNotFound', function(event, unfoundState, fromState, fromParams){
+                LoaderService.hide();
+                //event.preventDefault();
+                // transitionTo() promise will be rejected with
+                // a 'transition prevented' error
+                console.log('$stateNotFound');
+            });
+        }
+    };
+}
+
+
+ViewConfig.$inject = ["$templateCache"];
+angular
+    .module('da-loader')
+    .run(ViewConfig);
+
+function ViewConfig($templateCache){
+    $templateCache.put('id-loader/loader.html', '<div id="loading" ng-style="{\'display\': displayStatus()}"></div>');
+}
+})();
