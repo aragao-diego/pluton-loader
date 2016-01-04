a8b0e9eac4771d25cfe9ffbd332f13b239012981
Diego Silveira <diego.silveira@ctis.com.br>
2016-01-04 17:23:23 -0200

    Refatorando

--- src/da-loader.js
+++ src/da-loader.js
@@ -7,7 +7,7 @@ angular
     .module(module, ['ui.router', 'oc.lazyLoad'].concat(subModules) );
 
 function createSubModules(element, index, array){
-    var moduleName = module+element;
+    var moduleName = module+'.'+element;
     angular.module(moduleName, []);
     array[index] = moduleName;
 };
