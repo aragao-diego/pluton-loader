16b11a0d347c3a3f9b4a92d3bdbd8c4806390952
jenkins <jenkins@e167643ab065.(none)>
2016-01-18 16:58:32 -0200

    Release v0.0.46

--- dist/da-loader.js
+++ dist/da-loader.js
@@ -98,6 +98,93 @@ function DALoaderController($scope, $controller, $rootScope, LoaderService){
     }
 }
 
+(function(){
+    LoaderHttpController.$inject = ["$scope", "LoaderService", "Restangular"];
+    angular
+        .module('da-loader.controllers')
+        .controller('LoaderRestangularController', LoaderHttpController);
+
+    /* @ngInject */
+    function LoaderHttpController($scope, LoaderService, Restangular){
+        var vm = this;
+
+        vm.pendingRequests = 0;
+        vm.setUp = setUp;
+        vm.tearDown = tearDown;
+        vm.incrementRequest = incrementRequest;
+        vm.decrementRequest = decrementRequest;
+        vm.hasPendingRequests = hasPendingRequests;
+
+        ///////////
+        function setUp(){
+            Restangular.addFullRequestInterceptor(requestInterceptor);
+            Restangular.addResponseInterceptor(responseInterceptor);
+            Restangular.addErrorInterceptor(errorInterceptor);
+        }
+
+        function tearDown(){
+        }
+
+        function requestInterceptor(element, operation, what, url, headers, params, httpConfig) {
+            if(httpConfig && httpConfig.hasOwnProperty('da-loader') && httpConfig['da-loader'] === false ){
+                return element;
+            }
+
+            if (vm.pendingRequests === 0) {
+                LoaderService.enable();
+            }
+            vm.incrementRequest();
+            return element;
+        }
+
+        function responseInterceptor(data, operation, what, url, response, deferred) {
+            //Sem o fullResponse não tem como saber qual os headers do request
+            if(Restangular.configuration.fullResponse === false){
+                vm.decrementRequest();
+                if (!vm.hasPendingRequests()) {
+                    LoaderService.disable();
+                }
+                return data;
+            }
+
+            deferred.promise.then(checkForFullResponse);
+            return data;
+        }
+
+        function errorInterceptor(response, deferred, responseHandler) {
+            vm.decrementRequest();
+            if (!vm.hasPendingRequests()) {
+                LoaderService.disable();
+            }
+            return true; // error not handled
+        }
+
+        function checkForFullResponse(data){
+            if(data && data.config && data.config.hasOwnProperty('da-loader') && data.config['da-loader'] === false ){
+                return true;
+            }
+
+            vm.decrementRequest();
+            if (!vm.hasPendingRequests()) {
+                LoaderService.disable();
+            }
+        }
+
+        function incrementRequest(){
+            self.pendingRequests++;
+        }
+
+        function decrementRequest(){
+            self.pendingRequests--;
+        }
+
+        function hasPendingRequests(){
+            return self.pendingRequests > 0;
+        }
+    }
+
+})();
+
 
 LoaderNgRouterController.$inject = ["$scope", "$rootScope", "LoaderService"];angular
     .module('da-loader.controllers')
@@ -306,13 +393,46 @@ function LoaderDirective($rootScope, LoaderService, $parse){
     }
 }
 
+(function(){
+    LoaderHttpInterceptorFactory.$inject = ["$q", "LoaderService"];
+    angular
+        .module('da-loader.services')
+        .factory('daLoaderHttpInterceptor', LoaderHttpInterceptorFactory);
 
-LoaderService.$inject = ["$state"];angular
+    /*@ngInject*/
+    function LoaderHttpInterceptorFactory($q, LoaderService) {
+        return {
+            // optional method
+            'request': requestInterceptor,
+            'requestError': requestErrorInterceptor,
+            'response': responseInterceptor,
+            'responseError': reponseErrorInterceptor
+        };
+        ///////////////
+        function requestInterceptor(config){
+            return config;
+        }
+
+        function requestErrorInterceptor(rejection){
+            return rejection;
+        }
+
+        function responseInterceptor(response){
+            return response;
+        }
+
+        function reponseErrorInterceptor(rejection){
+            return rejection;
+        }
+    }
+})();
+
+angular
     .module('da-loader.services')
     .service('LoaderService', LoaderService );
 
 /* @ngInject */
-function LoaderService($state){
+function LoaderService(){
     var vm = this;
 
     var isShowing = false;
