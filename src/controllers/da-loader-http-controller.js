d016a9ec3bb22507ecdcace43dbffc85a0544b35
Diego Silveira <diego.silveira@ctis.com.br>
2016-01-18 18:01:14 -0200

    Renomeando definição do controlador

--- src/controllers/da-loader-http-controller.js
+++ src/controllers/da-loader-http-controller.js
@@ -1,19 +1,19 @@
 (function(){
     angular
         .module('da-loader.controllers')
-        .controller('LoaderRestangularController', LoaderHttpController);
+        .controller('LoaderHttpController', LoaderHttpController);
 
     /* @ngInject */
     function LoaderHttpController($scope, LoaderService){
         var vm = this;
 
         ///////////
+        //@TODO Registrar interceptor por meio de algum serviço (tempo de execução)
         function setUp(){
         }
 
+        //@TODO Desregistrar interceptor por meio de algum serviço (tempo de execução)
         function tearDown(){
         }
     }
-
-
 })();
