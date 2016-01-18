46e727a6a2d33f89cc4c72e4f6eef25e276c44c1
Diego Silveira <diego.silveira@ctis.com.br>
2016-01-18 18:06:02 -0200

    Implementando o contrato dos controlaores LoaderHttpController

--- src/controllers/da-loader-http-controller.js
+++ src/controllers/da-loader-http-controller.js
@@ -7,6 +7,9 @@
     function LoaderHttpController($scope, LoaderService){
         var vm = this;
 
+        vm.setUp = setUp;
+        vm.tearDown = tearDown;
+
         ///////////
         //@TODO Registrar interceptor por meio de algum serviço (tempo de execução)
         function setUp(){
