ee21453134b5f0cb6b48504f501aae812bf624ac
Diego Silveira <diego.silveira@ctis.com.br>
2016-01-18 17:53:27 -0200

    Interceptor http

--- src/controllers/da-loader-http-controller.js
+++ src/controllers/da-loader-http-controller.js
@@ -4,82 +4,16 @@
         .controller('LoaderRestangularController', LoaderHttpController);
 
     /* @ngInject */
-    function LoaderHttpController($scope, LoaderService, Restangular){
+    function LoaderHttpController($scope, LoaderService){
         var vm = this;
-
-        vm.pendingRequests = 0;
-        vm.setUp = setUp;
-        vm.tearDown = tearDown;
-        vm.incrementRequest = incrementRequest;
-        vm.decrementRequest = decrementRequest;
-        vm.hasPendingRequests = hasPendingRequests;
 
         ///////////
         function setUp(){
-            Restangular.addFullRequestInterceptor(requestInterceptor);
-            Restangular.addResponseInterceptor(responseInterceptor);
-            Restangular.addErrorInterceptor(errorInterceptor);
         }
 
         function tearDown(){
         }
-
-        function requestInterceptor(element, operation, what, url, headers, params, httpConfig) {
-            if(httpConfig && httpConfig.hasOwnProperty('da-loader') && httpConfig['da-loader'] === false ){
-                return element;
-            }
-
-            if (vm.pendingRequests === 0) {
-                LoaderService.enable();
-            }
-            vm.incrementRequest();
-            return element;
-        }
-
-        function responseInterceptor(data, operation, what, url, response, deferred) {
-            //Sem o fullResponse não tem como saber qual os headers do request
-            if(Restangular.configuration.fullResponse === false){
-                vm.decrementRequest();
-                if (!vm.hasPendingRequests()) {
-                    LoaderService.disable();
-                }
-                return data;
-            }
-
-            deferred.promise.then(checkForFullResponse);
-            return data;
-        }
-
-        function errorInterceptor(response, deferred, responseHandler) {
-            vm.decrementRequest();
-            if (!vm.hasPendingRequests()) {
-                LoaderService.disable();
-            }
-            return true; // error not handled
-        }
-
-        function checkForFullResponse(data){
-            if(data && data.config && data.config.hasOwnProperty('da-loader') && data.config['da-loader'] === false ){
-                return true;
-            }
-
-            vm.decrementRequest();
-            if (!vm.hasPendingRequests()) {
-                LoaderService.disable();
-            }
-        }
-
-        function incrementRequest(){
-            self.pendingRequests++;
-        }
-
-        function decrementRequest(){
-            self.pendingRequests--;
-        }
-
-        function hasPendingRequests(){
-            return self.pendingRequests > 0;
-        }
     }
+
 
 })();
--- src/services/da-loader-http-interceptor-service.js
+++ src/services/da-loader-http-interceptor-service.js
@@ -5,15 +5,27 @@
 
     /*@ngInject*/
     function LoaderHttpInterceptorFactory($q, LoaderService) {
-        return {
+        var interceptor = {
             // optional method
             'request': requestInterceptor,
             'requestError': requestErrorInterceptor,
             'response': responseInterceptor,
-            'responseError': reponseErrorInterceptor
+            'responseError': reponseErrorInterceptor,
+            'pendingRequests': 0
         };
+        return interceptor;
+
         ///////////////
         function requestInterceptor(config){
+            if(notUseLoader(config.headers)){
+                return config;
+            }
+
+            if (interceptor.pendingRequests === 0) {
+                LoaderService.enable();
+            }
+            interceptor.incrementRequest();
+
             return config;
         }
 
@@ -22,11 +34,49 @@
         }
 
         function responseInterceptor(response){
+            if(notUseLoader(response.config.headers)){
+                return response;
+            }
+
+            interceptor.decrementRequest();
+            if (!interceptor.hasPendingRequests()) {
+                LoaderService.disable();
+            }
+
             return response;
         }
 
         function reponseErrorInterceptor(rejection){
+            if(notUseLoader(rejection.config.headers)){
+                return rejection;
+            }
+
+            interceptor.decrementRequest();
+            if (!interceptor.hasPendingRequests()) {
+                LoaderService.disable();
+            }
+
             return rejection;
+        }
+
+        function notUseLoader(data){
+            if(data && data.hasOwnProperty('da-loader') && data['da-loader'] === false ){
+                return true;
+            }
+
+            return false;
+        }
+
+        function incrementRequest(){
+            interceptor.pendingRequests++;
+        }
+
+        function decrementRequest(){
+            interceptor.pendingRequests--;
+        }
+
+        function hasPendingRequests(){
+            return interceptor.pendingRequests > 0;
         }
     }
 })();
